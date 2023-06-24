5c31ee1edbdc670e004acb9203eca6ccf6e93b1e
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-06-24 11:37:21 +0100

    Add basic routes using BrowserRouter, Switch and Route

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/extensions': 0,
+    'react/prop-types': 0,
+    'linebreak-style': 0,
+    'react/state-in-constructor': 0,
+    'import/prefer-default-export': 0,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'max-len': [2, 250],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 1,
+      },
+    ],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: ['_d', '_dh', '_h', '_id', '_m', '_n', '_t', '_text'],
+      },
+    ],
+    'object-curly-newline': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/alt-text': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'react/no-array-index-key': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to', 'hrefLeft', 'hrefRight'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <>
-      <div>App</div>
-    </>
-  );
-};
+const App = () => (
+  <div>
+    <CssBaseline />
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <h1>Movies</h1>
+        </Route>
+        <Route exact path="/movie/:id">
+          <h1>Movie Information</h1>
+        </Route>
+        <Route exact path="/actors">
+          <h1>Actors</h1>
+        </Route>
+        <Route exact path="/profile/:id">
+          <h1>Profile</h1>
+        </Route>
+      </Switch>
+    </main>
+  </div>
+);
 
 export default App;
