fcf69312f09ce85bed536dad473f96b15ff50f41
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-06-24 15:37:29 +0100

    :lipstick: Added vaery basic base styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,35 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 import { Navbar, Movies, MovieInformation, Actors, Profile } from '.';
+import useStyles from './styles';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <Navbar />
+const App = () => {
+  const classes = useStyles();
 
-    <main>
-      <Switch>
-        <Route exact path="/">
-          <Movies />
-        </Route>
-        <Route exact path="/movie/:id">
-          <MovieInformation />
-        </Route>
-        <Route exact path="/actors">
-          <Actors />
-        </Route>
-        <Route exact path="/profile/:id">
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
