21f91e7f9e598990cf875610aed800824471cd70
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-06-24 17:50:04 +0100

    :sparkles: Now have a basic navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
+import { AppBar, Avatar, Button, Drawer, IconButton, Toolbar, useMediaQuery } from '@mui/material';
+import { AccountCircle, Brightness4, Brightness7, Menu } from '@mui/icons-material';
+import { useTheme } from '@mui/styles';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
 const Navbar = () => {
-  console.log('Navbar');
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const isAuthenticated = true;
 
   return (
-    <div>Navbar</div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{ outline: 'none' }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+
+          <IconButton
+            color="inherit"
+            sx={{ ml: 1 }}
+            onClick={() => {}}
+          >
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+
+          {!isMobile && 'Search ...'}
+
+          <div>
+            {!isAuthenticated ? (
+              <Button
+                color="inherit"
+                onClick={() => {}}
+              >
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/:id"
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp; </>}
+
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="User profile picture"
+                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search ...'}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
+const theme = createTheme({});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 
   document.getElementById('root'),
 );
