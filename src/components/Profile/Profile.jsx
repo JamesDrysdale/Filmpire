afaefbce4f08d6654e63e16d2595210ca866ae17
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-06-30 09:24:44 +0100

    Added heading and logout button to profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Box, Button, Typography } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
 
 const Profile = () => {
@@ -8,7 +10,17 @@ const Profile = () => {
   console.log('Profile');
 
   return (
-    <div>{user.name ? user.name : user.username}</div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {/* {user.name ? user.name : user.username} */}
+    </Box>
   );
 };
 
