86b90f1e3deab10dcf4f5ccd9128093651586527
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-06-30 10:59:23 +0100

    Users can see the movie poster on the movie details page

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -8,7 +8,7 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
-    //* Get Genres
+    // Get Genres
     getGenres: builder.query({
       query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
     }),
@@ -34,10 +34,15 @@ export const tmdbApi = createApi({
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+    // Get movie
+    getMovie: builder.query({
+      query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
 export const {
   useGetGenresQuery,
   useGetMoviesQuery,
+  useGetMovieQuery,
 } = tmdbApi;
