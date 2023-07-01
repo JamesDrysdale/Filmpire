a37aff0c7b155dde038ff97e8ef9ba515c34058e
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-07-01 23:22:44 +0100

    Made actor profile images more responsive to display better on smaller devices

--- src/components/Actors/styles.js
+++ src/components/Actors/styles.js
@@ -1,10 +1,16 @@
 import { makeStyles } from '@mui/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   image: {
-    maxWidth: '90%',
+    maxWidth: '80%',
     borderRadius: '20px',
     objectFit: 'cover',
     boxShadow: '0.5em 0.5em 1em',
+    [theme.breakpoints.down('md')]: {
+      display: 'flex',
+      margin: '0 auto',
+      marginBottom: '30px',
+      width: '50%',
+    },
   },
 }));
