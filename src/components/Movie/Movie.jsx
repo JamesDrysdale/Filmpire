e44e33217e343629ad4a532d4941d3c16f481a0e
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-07-04 22:42:10 +0100

    Fixed Grow bug

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import { Grid, Grow, Rating, Tooltip, Typography } from '@mui/material';
 import useStyles from './styles';
 
-const Movie = ({ movie, i }) => {
+const Movie = ({ movie, index }) => {
   const classes = useStyles();
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-      <Grow in key={i} timeout={(i + 1) * 250}>
+      <Grow in key={index} timeout={(index + 1) * 250}>
         <Link className={classes.links} to={`/movie/${movie.id}`}>
           <img
             alt={movie.title}
