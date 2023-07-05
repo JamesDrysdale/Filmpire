8ceac106727c095fbcd1ca9efde9cfe2e4b659e5
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-07-05 10:44:37 +0100

    Added Alan AI button to the UI and implemented first two interactions

---
+++ src/components/Alan.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useContext } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { ColorModeContext } from '../utils/ToggleColorMode';
+
+const useAlan = () => {
+  const { setMode } = useContext(ColorModeContext);
+
+  useEffect(() => {
+    alanBtn({
+      key: 'c0a2e61e7f025ba51c91b10ed241252a2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, mode }) => {
+        if (command === 'changeMode') {
+          if (mode === 'light') {
+            setMode('light');
+          } else {
+            setMode('dark');
+          }
+        }
+      },
+    });
+  }, []);
+};
+
+export default useAlan;
