fde976098b2d861a37edc46c42d275e6c7771817
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2023-07-15 17:29:06 +0100

    Users tmdb avatars are now used instead of a default image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
                 <Avatar
                   style={{ width: 30, height: 30 }}
                   alt="User profile picture"
-                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+                  src={`https:www.themoviedb.org/t/p/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
                 />
               </Button>
             )}
